f1b27c628eb5d3ee6dbcbdc1badd8f231e43bdd8
OgGhostJelly <johnnycambodia09@gmail.com>
2023-01-18 08:59:22 +0700

    added object drop array and moving

--- 2d/interactable/breakable.gd
+++ 2d/interactable/breakable.gd
@@ -4,9 +4,8 @@ class_name Breakable
 
 @export var max_health: float = 3.0
 @export var health: float = 3.0
-@export var dropped_item: PackedScene
-@export var drop_rate: Vector2i = Vector2i.ONE
-@export var drop_spread: float = 20.0
+@export var drops: Array[PackedScene]
+@export var spread: float = 30.0
 
 
 
@@ -19,10 +18,10 @@ func _interacted() -> void:
 
 
 func drop_items() -> void:
-	for i in randi_range(drop_rate.x, drop_rate.y): drop_item()
+	for x in drops: drop_item(x)
 
 
-func drop_item() -> void:
-	var obj: Node = dropped_item.instantiate()
-	obj.global_position = global_position + Vector2(randf_range(-drop_spread, drop_spread), randf_range(-drop_spread, drop_spread))
+func drop_item(item: PackedScene) -> void:
+	var obj: Node = item.instantiate()
+	obj.global_position = global_position + Vector2(randf_range(-spread, spread), randf_range(-spread, spread))
 	get_tree().current_scene.add_child(obj)
